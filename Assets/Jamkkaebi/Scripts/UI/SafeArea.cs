15d6260eedd7af29f311d68510fa1cf6fdf216fe
thfdl <soliepark9204@cau.ac.kr>
2026-08-30 23:42:10 +0900

    feat: SafeArea 스크립트 추가
    안전영역을 RectTransform으로 변환하는 클래스

---
+++ Assets/Jamkkaebi/Scripts/UI/SafeArea.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeArea : MonoBehaviour
+{
+    private RectTransform _rectTransform;
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        ApplySafeArea();
+    }
+
+    private void ApplySafeArea()
+    { 
+        Rect safeArea = Screen.safeArea;
+
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+        
+        _rectTransform.anchorMin = anchorMin;
+        _rectTransform.anchorMax = anchorMax;
+    }
+}
