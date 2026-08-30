64e8027d18d5baeb87064958219cf876767fcf2f
thfdl <soliepark9204@cau.ac.kr>
2026-08-31 04:02:53 +0900

    feat: 타일 데이터 구조 정의

---
+++ Assets/Jamkkaebi/Scripts/Gameplay/Minigame/Tile.cs
@@ -0,0 +1,27 @@
+namespace Jamkkaebi.Scripts.Gameplay.Minigame
+{
+    public class Tile
+    {
+        public TileContent Content { get; }
+        public bool IsRevealed { get; private set; }
+        public bool IsReinforced { get; private set; }
+
+        public Tile(TileContent content, bool isReinforced = false)
+        {
+            Content = content;
+            IsRevealed = false;
+            IsReinforced = isReinforced;
+        }
+
+        public void Reveal()
+        {
+            if (IsReinforced)
+            {
+                IsReinforced = false;
+                return;
+            }
+            
+            IsRevealed = true;
+        }
+    }
+}
